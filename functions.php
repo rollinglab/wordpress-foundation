b73aa4e7816ebd171042882be3c2df7573547c86
paulruescher <paul@rollinglab.com>
2013-04-24 15:28:06 -0500

    changes

--- functions.php
+++ functions.php
@@ -2,12 +2,6 @@
 
 require_once( get_template_directory() . '/inc/breadcrumbs.php' );
 
-/**
- * Set the content width based on the theme's design and stylesheet.
- */
-if ( ! isset( $content_width ) )
-	$content_width = 640; /* pixels */
-
 /*
  * Load Jetpack compatibility file.
  */
