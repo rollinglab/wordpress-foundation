7d0ced635933e96c03c49d775eb4f81adbd3303d
paulruescher <paul@rollinglab.com>
2013-04-24 18:41:07 -0500

    fuck yeah buttons!  Buttons shortcode

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -1,7 +1,34 @@
 <?php
 
+
+/*
+ * Button Shortcode
+ *
+ */
+function lab_button( $atts, $content = '' ) {
+     extract( shortcode_atts ( array(
+          'url'     => '',
+          'size'    => 'medium',
+          'shape'   => '',
+          'type'    => 'primary'
+     ), $atts ) );
+
+     $size  = ( !empty( $size ) ? ' ' . $size : '' );
+     $shape = ( !empty( $shape ) ? ' ' . $shape : '' );     
+     $type  = ( !empty( $type ) ? ' ' . $type : '' );
+     
+     // build class string from input values     
+     $class = 'button' . $type . $size . $shape;
+     
+     return '<a href="' . $url . '" class="' . $class . '">' . $content . '</a>';
+         
+}
+add_shortcode( 'button', 'lab_button' );
+
+
 /*
  * Creates <div class="row">
+ *
  */
 function lab_grid_row( $atts, $content = '' ) {
      extract( shortcode_atts ( array(
@@ -22,15 +49,24 @@ add_shortcode( 'grid-row', 'lab_grid_row' );
  */
 function lab_grid_column( $atts, $content = '' ) {
      extract( shortcode_atts ( array(
-          'columns' => '12',
-          'offset'  => '',
-          'size'    => 'large'
+          'columns'  => '12',
+          'offset'   => '',
+          'pull'     => '',
+          'push'     => '',
+          'center'   => '',
+          'size'     => 'large'
      ), $atts ) );
      
-     $columns = $size . '-' . $columns;
-     $offset = $size . '-offset-' . $offset;     
+     $columns  = $size . '-' . $columns;
+     $offset   = ( !empty( $offset ) ? ' ' . $size . '-offset-' . $offset : '');
+     $center   = ( !empty( $center ) ? ' ' . $size . '-centered' : '' );
+     $push     = ( !empty( $push ) ? ' push-' . $push : '' );
+     $pull     = ( !empty( $pull ) ? ' pull-' . $pull : '' );
      
-     return '<div class="' . $columns . ' columns ' . $offset . '">' . do_shortcode( $content ) . '</div>';
+     // build class string from input values
+     $class = $columns . ' columns' . $offset . $center . $push . $pull;
+     
+     return '<div class="' . $columns . '">' . do_shortcode( $content ) . '</div>';
          
 }
 add_shortcode( 'grid-column', 'lab_grid_column' );
@@ -38,6 +74,7 @@ add_shortcode( 'grid-column', 'lab_grid_column' );
 
 /*
  * Used to display shortcode examples
+ *
  */
 function lab_code( $atts, $content = '' ) {
      
